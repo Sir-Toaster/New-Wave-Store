79408709f8d4ae7b35f3cfc934372e8329341418
Sir-Toaster <117207202+Sir-Toaster@users.noreply.github.com>
2022-11-18 13:12:36 -0500

    Update ContentView.swift

--- New Wave Store/ContentView.swift
+++ New Wave Store/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Farewell all those close and dear to me!!")
             .padding()
     }
 }
